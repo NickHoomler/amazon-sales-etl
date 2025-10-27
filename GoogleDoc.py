30fcc4563d4297e785b91b603dd75a50c04b8f91
NickHoomler <no3736972@gmail.com>
2025-10-28 00:49:50 +0300

    Update GoogleDoc.py

--- GoogleDoc.py
+++ GoogleDoc.py
@@ -7,21 +7,20 @@ import os
 
 # Засекаем общее время выполнения
 start_time = time.time()
-
 # 1. Настройки подключения
-SHEET_URL = os.environ['SHEET_URL']  # Берём из секретов
-SHEET_NAME_hansa = "HansaPart"
-SHEET_NAME_vioks = "VIOKS"
-SHEET_NAME_grtrade = "GR_Trade"
-SHEET_NAME_Olev = "Olev"
-SHEET_NAME_Metro = "METRO"
-SHEET_NAME_Shegira = "Shegira"
-SHEET_NAME_Ebay = "eBay"
-SHEET_NAME_otto = "OTTO"
-SHEET_NAME_kaufland = "Kaufland.de"
-SHEET_NAME_woocommerce = "woocommerceErsatzteilCheck"
-SHEET_NAME_woocommerce_aquade = "woocommerceAQUADE"
-SHEET_NAME_allegro = "Allegro"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1ahL4ku_1I7JGX9w9evF-L9W3hZKZ9xa37gldA4czz1w"
+SHEET_NAME_hansa = "HansaPart Алиса Рожина"  # Название нужной вкладки
+SHEET_NAME_vioks = "VIOKS Тамара Макушенко"
+SHEET_NAME_grtrade = "GR-Trade Ольга Полуектова"
+SHEET_NAME_Olev = "OLEV Ольга Грекова"
+SHEET_NAME_Metro = "METRO Анастасия Болдова"
+SHEET_NAME_Shegira = "Shegira Тамара Макушенко"
+SHEET_NAME_Ebay = "eBay Екатерина Колодяжная"
+SHEET_NAME_otto = "OTTO Ирина Бергер"
+SHEET_NAME_kaufland = "Kaufland.de Алиса Рожина"
+SHEET_NAME_woocommerce = "wwoocommerceErsatzteilCheck Алиса Рожина"
+SHEET_NAME_woocommerce_aquade = "woocommerceAQUADE Алиса Рожина"
+SHEET_NAME_allegro = "Allegro Алина Роек"
 
 # Подключение для страницы Hanza ----------------------------------------------
 csv_url_hansa = f"{SHEET_URL}/gviz/tq?tqx=out:csv&sheet={urllib.parse.quote(SHEET_NAME_hansa)}"
@@ -56,7 +55,7 @@ df_grtrade['Company_id'] = '1511'
 
 # Подключение Olev-------------------------------------------------------------
 csv_url_olev = f"{SHEET_URL}/gviz/tq?tqx=out:csv&sheet={urllib.parse.quote(SHEET_NAME_Olev)}"
-df_olev = pd.read_csv(csv_url_olev)
+df_olev= pd.read_csv(csv_url_olev)
 
 df_olev.rename(columns={
     'Sales €': 'Sales',
@@ -68,7 +67,7 @@ df_olev['Company_id'] = '8'
 
 # Подключение Metro -----------------------------------------------------------
 csv_url_metro = f"{SHEET_URL}/gviz/tq?tqx=out:csv&sheet={urllib.parse.quote(SHEET_NAME_Metro)}"
-df_metro = pd.read_csv(csv_url_metro)
+df_metro= pd.read_csv(csv_url_metro)
 
 df_metro['Платформа'] = 'metro'
 df_metro['Company_id'] = '1'
@@ -151,7 +150,7 @@ df_erz.rename(columns={
 df_erz['Платформа'] = 'aquade'
 df_erz['Company_id'] = '1'
 
-# Собираем финальный DataFrame------------------------------------------------
+# Собираем финальный DateFrame------------------------------------------------
 df_final = []
 
 df_final.append(df_hansa)
@@ -169,18 +168,16 @@ df_final.append(df_erz)
 
 combined_df = pd.concat(df_final, ignore_index=True)
 
-# Оставляем только нужные колонки
-necessary_columns = ['Date', 'Sales', 'Units', 'Inactive', 'Active', 
+nessery_columns = ['Date', 'Sales', 'Units', 'Inactive', 'Active', 
                    'Платформа', 'Company_id', 'Залистовано', 
-                   'Оптимизировано', 'Returns']  # Убрал 'Active ' с пробелом
+                   'Оптимизировано', 'Active ', 'Returns']
 
-available_columns = [col for col in necessary_columns if col in combined_df.columns]
+
+available_columns = [col for col in nessery_columns if col in combined_df.columns]
 combined_df = combined_df[available_columns]
-
 # 3. Пример использования
 print(combined_df.head())
-print("Доступные колонки:", combined_df.columns.tolist())
-print(f"Всего строк: {len(combined_df)}")
+print(combined_df.columns.tolist())
 
 # Учетные данные из секретов
 DB_CONFIG = {
